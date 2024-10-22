c8c8a6cd4df90a7d9e46ff3b6435726ae4f179ba
Kaylem Williams <kaylemw1@outlook.com>
2024-10-22 13:30:40 +1300

    draw grid

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+const GRID_WIDTH = 30
+const GRID_HEIGHT = 20
 
-# Called when the node enters the scene tree for the first time.
+@onready var tile_map_layer = $TileMapLayer
+@onready var camera = $Camera2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	draw_grid()
+	center_camera()
 
+func center_camera():
+	var size = Vector2i(GRID_WIDTH, GRID_HEIGHT) * tile_map_layer.tile_set.tile_size
+	camera.set_position(Vector2(size.x / 2, size.y / 2))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func draw_grid() -> void:
+	# Draw base grid
+	for x in range(0, GRID_WIDTH):
+		for y in range(0, GRID_HEIGHT):
+			tile_map_layer.set_cell(Vector2i(x, y), 1, Vector2i(0, 0), 0)
+	
